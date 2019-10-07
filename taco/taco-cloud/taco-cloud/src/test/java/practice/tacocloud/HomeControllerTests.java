d13b707e419e4286017d786c29a2ed8788f8c89f
Luke Brannigan <lukebrannigan57@gmail.com>
2019-10-07 19:06:21 -0400

    created controller and template and controller template class

---
+++ taco/taco-cloud/taco-cloud/src/test/java/practice/tacocloud/HomeControllerTests.java
@@ -0,0 +1,13 @@
+package practice.tacocloud;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMVCTest(HomeController.class)
+
+
+public class HomeControllerTests {
+	
+
+}
